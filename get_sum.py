7f9eca594a05dfb864cbce5c51bd2c1dee04e211
Rugby Granite2 (SUMM-IT) <rugby.granite2@vantiva.com>
2023-09-15 14:29:55 +0100

    commit

--- get_sum.py
+++ get_sum.py
@@ -1,5 +1,17 @@
 def get_sum(a,b):
-    if a != b:
-        return a + b
-    else:
-        return a
+    sum = 0
+    if a < b:
+        while a <= b:
+            sum += a
+            a += 1
+    elif b < a:
+        while b <= a:
+            sum += b
+            b += 1
+    elif a == b:
+        sum = a
+
+    return sum
+
+
+get_sum(1, 3)
