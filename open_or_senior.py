b592e60c0a73b477a13e1f687cb25143e6a6ebce
Rugby Granite2 (SUMM-IT) <rugby.granite2@vantiva.com>
2023-09-15 13:18:30 +0100

    commit

--- open_or_senior.py
+++ open_or_senior.py
@@ -1,7 +1,7 @@
 def open_or_senior(data):
     status = []
     for i in range (0, len(data)):
-        if data[i][0] >= 55 and data[i][1] >= 7:
+        if data[i][0] >= 55 and data[i][1] > 7:
             status.append("Senior")
         else:
             status.append("Open")
