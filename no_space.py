406f210cbf15247bc55e6c1705cbceefd410ded0
Rugby Granite2 (SUMM-IT) <rugby.granite2@vantiva.com>
2023-09-18 08:59:21 +0100

    commit

---
+++ no_space.py
@@ -0,0 +1,9 @@
+def no_space(x):
+    x = x.split()
+    x = ''.join(x)
+    return x
+
+
+
+
+no_space('8 j 8   mBliB8g  imjB8B8  jl  B')
---
+++ string_to_array.py
@@ -0,0 +1,11 @@
+def string_to_array(s):
+    return s.split(' ')
+
+
+
+
+
+
+
+
+string_to_array("")
